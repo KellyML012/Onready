90a693311553d61e91f8529db5a9157078c3edeb
Raquel <59716516+KellyML012@users.noreply.github.com>
2020-06-16 18:44:24 -0300

    Update Auto.java

--- Auto.java
+++ Auto.java
@@ -1,7 +1,7 @@
 
 public class Auto extends Vehiculo{
 	
-	protected int puertas;
+	private int puertas;
 	
 	public Auto(String marca, String modelo, int puertas, double precio) {
 		super(marca, modelo, precio);
